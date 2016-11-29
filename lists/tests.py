db208da3079faeab7aa32edf21d86ab20e36b473
samaras <skomfi@gmail.com>
2016-11-29 20:28:25 +0200

    Updated test.py

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,10 @@ class HomePageTest(TestCase):
 	def test_home_page_url_and_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>Bytestore</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
