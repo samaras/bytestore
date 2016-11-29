7f890baad796fa4e198668cd07d8866c01fae5a2
samaras <skomfi@gmail.com>
2016-11-29 16:08:35 +0200

    First unit test and url mapping, dummy home view

--- bytestore/urls.py
+++ bytestore/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'bytestore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- lists/tests.py
+++ lists/tests.py
@@ -2,12 +2,6 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 
-class SmokeTest(TestCase):
-	"""docstring for SmokeTest"""
-	
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
-
 class HomePageTest(TestCase):
 
 	def test_home_page_url_and_view(self):
