885eb6499093e798d26212e0854b4b55baee45f7
samaras <skomfi@gmail.com>
2016-11-29 13:03:21 +0200

    Home Page Tests and View

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 class SmokeTest(TestCase):
 	"""docstring for SmokeTest"""
@@ -6,4 +8,8 @@ class SmokeTest(TestCase):
 	def test_bad_maths(self):
 		self.assertEqual(1 + 1, 3)
 
-		
+class HomePageTest(TestCase):
+
+	def test_home_page_url_and_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
