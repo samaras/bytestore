6d3811255e2f1ea1d832b1ac6c1f169a0bbd225a
samaras <skomfi@gmail.com>
2016-11-29 12:28:54 +0200

    Added lists app, Changed DB settings, Modified functional test to check title and header

--- bytestore/settings.py
+++ bytestore/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'bytestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bytestore',
+        'USER': 'root',
+        'PASSWORD': 'sam123kom'
     }
 }
 
--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,21 @@
-from selenium import webdriver
+from selenium import webdriver 
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setup(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get("http://localhost:8083")
+
+		# Check page title and header
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main(warinings='ignore')
