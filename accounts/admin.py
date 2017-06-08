0800bb4cb2e62c351f782c1aa7377593a9b0d5cc
samaras <skomfi@gmail.com>
2017-06-08 19:50:41 +0200

    Added Cart & Accounts admin.py file, some css changes for alerts and notifications

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _ 
+
+from accounts.models import Profile
 
 # Register your models here.
+
+class ProfileInline(admin.StackedInline):
+	model = Profile
+	can_delete = False
+	verbose_name_plural = _('employee')
+
+class UserAdmin(BaseUserAdmin):
+	inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
